3556b456bc38444cadc5d21afcb322b57efb132b
Sh1fue <110296436+Sh1fue@users.noreply.github.com>
2023-11-15 19:31:41 +0300

    Update Contents.swift

--- 12-ya/6.playground/Contents.swift
+++ 12-ya/6.playground/Contents.swift
@@ -3,7 +3,7 @@
 //    var FIO: String
 //    var Sport: String
 //    var Age: Int
-//    
+//
 //    init(FIO: String, Sport: String, Age: Int) {
 //        self.FIO = FIO
 //        self.Sport = Sport
@@ -13,13 +13,13 @@
 //
 //class Sportsmens {
 //    var sports: [Sportsmen] = [Sportsmen]()
-//    
+//
 //    init() {
 //        sports.append(Sportsmen(FIO: "Черкасов Вадим Андреевич", Sport: "Футбол", Age: 17))
 //        sports.append(Sportsmen(FIO: "Железнов Вадим Александрович", Sport: "Баскетбол", Age: 20))
 //        sports.append(Sportsmen(FIO: "Крайнюков Александр Александрович", Sport: "Тенис", Age: 16))
 //    }
-//    
+//
 //    subscript(index: Int) -> Sportsmen {
 //        get {
 //            return sports[index]
@@ -40,43 +40,34 @@
 //}
 //
 //last task
-//class error {
-//    var date:String
-//    var message:String
-//    var errorCode:Int
-//    
-//init(date:String, message:String, errorCode:Int) {
-//    self.date = date
-//    self.message = message
-//    self.errorCode = errorCode
-//    }
-//}
-//
-//class ErrorWatcher {
-//var errors:[error]
-//    
-//init(errors:[error]) {
-//    self.errors = errors
-//}
-//    
-//subscript(time:String) -> [error] {
-//    get {
-//        var errorsOnChangedData:[error] = []
-//        for error in self.errors {
-//            if error.date == time {
-//                errorsOnChangedData.append(error)
-//            }
-//        }
-//        return errorsOnChangedData
-//    }
-//}}
-//
-//let error1 = error(date: "30 Сентября 2022", message: "Ресурс не найден", errorCode:
-//404)
-//let error2 = error(date: "7 Октября 2022", message: "Сервис недоступен", errorCode: 503)
-//let error3 = error(date: "18 Октября 2022", message: "Внутренняя ошибка сервера",
-//errorCode: 500)
-//let errors: [error] = [error1, error2, error3]
-//
-//var watcher = ErrorWatcher(errors:errors)
-//print(watcher["7 Октября 2022"][0].message)
+struct CustomError {
+    var date: String
+    var message: String
+    var errorCode: Int
+}
+
+class ErrorTracker {
+    var errors: [CustomError]
+
+    init(errors: [CustomError]) {
+        self.errors = errors
+    }
+
+    subscript(time: String) -> [CustomError] {
+        return errors.filter { $0.date == time }
+    }
+}
+
+let error1 = CustomError(date: "13 ноября 2023", message: "Ресурс не найден", errorCode: 404)
+let error2 = CustomError(date: "12 ноября 2023", message: "Сервис недоступен", errorCode: 503)
+let error3 = CustomError(date: "10 ноября 2023", message: "Внутренняя ошибка сервера", errorCode: 500)
+let errors: [CustomError] = [error1, error2, error3]
+
+var errorTracker = ErrorTracker(errors: errors)
+
+// Check if there are errors for the given date before accessing the message property
+if let firstError = errorTracker["7 Октября 2022"].first {
+    print(firstError.message)
+} else {
+    print("No error found")
+}
